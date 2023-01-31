25d7342ef2d87ab0084ef323cd757fa2ac6aad3e
F-17-2006 <3375926016@qq.com>
2023-01-31 21:33:30 +0800

    添加活动静态页

--- miniprogram/pages/activity/index.js
+++ miniprogram/pages/activity/index.js
@@ -10,14 +10,15 @@ Page({
     // 切换swiper-item触发bindchange事件
     pagechange: function (e) {
         // 通过touch判断，改变tab的下标值
-        if ("touch" === e.detail.source) {
-        let currentPageIndex = this.data.currentIndex;
-        currentPageIndex = (currentPageIndex + 1) % 2;
+        // console.log(e);
+        // if ("touch" === e.detail.source) {
+        // let currentPageIndex = this.data.currentIndex;
+        // currentPageIndex = (currentPageIndex + 1) % 2;
         // 拿到当前索引并动态改变
         this.setData({
-            currentIndex: currentPageIndex,
+            currentIndex: e.detail.current,
         })
-        }
+        // }
     },
 
     //点击tab时触发
