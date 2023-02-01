786259076a6f7013b5ac382ad4449852a28a4851
lwb <1375229126@qq.com>
2023-02-01 22:03:12 +0800

    写好了审核的页面

---
+++ miniprogram/pages/mine/audit/index.js
@@ -0,0 +1,85 @@
+// pages/mine/audit/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        currentIndex: 0, //默认是活动项
+    },
+    pagechange: function (e) {
+        // 通过touch判断，改变tab的下标值
+        // console.log(e);
+        // if ("touch" === e.detail.source) {
+        // let currentPageIndex = this.data.currentIndex;
+        // currentPageIndex = (currentPageIndex + 1) % 2;
+        // 拿到当前索引并动态改变
+        this.setData({
+            currentIndex: e.detail.current,
+        })
+        // }
+    },
+    //点击tab时触发
+    titleClick: function (e) {
+        this.setData({
+        //拿到当前索引并动态改变
+        currentIndex: e.currentTarget.dataset.idx
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})
