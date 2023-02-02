46afb0eb6a7b8bc305d9f5374697a3689a70dd0e
F-17-2006 <3375926016@qq.com>
2023-02-02 11:36:08 +0800

    添加activityTtile组件

---
+++ miniprogram/components/activityTitle/index.js
@@ -0,0 +1,30 @@
+// components/activityTitle/index.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        color:{
+            type:String,
+            value:'black'
+        },
+        spotColor:{
+            type:String,
+            value:'block'
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
