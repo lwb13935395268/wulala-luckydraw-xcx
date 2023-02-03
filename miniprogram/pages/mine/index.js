93eabf9a1d84daaa5dbe22f7d7800a304195721b
yang <1145938287@qq.com>
2023-02-03 17:22:45 +0800

    mine

--- miniprogram/pages/mine/index.js
+++ miniprogram/pages/mine/index.js
@@ -1,23 +1,63 @@
 // pages/mine/index.js
 Page({
-    navgator(e){
-        let path=e.currentTarget.dataset.path;
+    navgator(e) {
+        let path = e.currentTarget.dataset.path;
         wx.navigateTo({
-          url: '/pages/mine/'+path,
+            url: '/pages/mine/' + path,
         })
+    },
+    async getUserInfo() {
+        let wxspAppid = 'wx525528379d006020';
+        let wxspSecret = '';
+        // wx.login({
+        //     success(res) {
+        //         if (res.code) {
+        //             //发起网络请求
+        //             wx.request({
+        //                 url: "https://api.weixin.qq.com/sns/jscode2session?appid=" + wxspAppid + "&secret=" + wxspSecret + "&js_code=" + res.code + "&grant_type=authorization_code",
+        //                 method: "POST",
+        //                 success: (res) => {
+        //                     console.log(res);
+        //                 }
+        //             })
+        //         } else {
+        //             console.log('登录失败！' + res.errMsg)
+        //         }
+        //     }
+        // })
+        let {
+            getUserInfo,
+        } = getApp();
+        let {
+            userInfo
+        } = await getUserInfo('我的页面登录');
+        if (userInfo) {
+            this.setData({
+                userInfo
+            });
+            console.log(userInfo);
+        }
     },
     /**
      * 页面的初始数据
      */
-    data: {
-
-    },
+    data: {},
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(options) {
-
+        wx.setNavigationBarTitle({
+            title: '我的'
+        })
+        wx.setNavigationBarColor({
+            frontColor: '#ffffff',
+            backgroundColor: '#eb524c',
+            animation: {
+                duration: 400,
+                timingFunc: 'easeIn'
+            }
+        })
     },
 
     /**
