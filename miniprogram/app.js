a58dd0c35932edf3198d11cbca77e4ec97ca1024
F-17-2006 <3375926016@qq.com>
2023-02-13 08:27:16 +0800

    配置下拉刷新

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -158,5 +158,18 @@ App({
                 }
             })
         })
-    }
+    },
+    // 下拉刷新
+    onPullDownRefresh:function(){
+        this.onRefresh();
+      },
+    onRefresh:function(){
+        //导航条加载动画
+        wx.showNavigationBarLoading();
+        setTimeout(function () {
+          wx.hideNavigationBarLoading();
+          //停止下拉刷新
+          wx.stopPullDownRefresh();
+        }, 500);
+    },
 });
--- miniprogram/pages/activity/index.js
+++ miniprogram/pages/activity/index.js
@@ -174,7 +174,11 @@ Page({
      * 页面相关事件处理函数--监听用户下拉动作
      */
     onPullDownRefresh() {
-
+        wx.showLoading({
+            title: '加载中',
+            mask: true,
+        });
+        this.wholeActivity();
     },
 
     /**
