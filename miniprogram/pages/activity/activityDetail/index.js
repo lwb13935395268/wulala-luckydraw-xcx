095982fb7bf41dd6ba6be187f4ce852c95d356cf
lwb <1375229126@qq.com>
2023-02-14 22:02:26 +0800

    修改了活动详情页面的bug

--- miniprogram/pages/activity/activityDetail/index.js
+++ miniprogram/pages/activity/activityDetail/index.js
@@ -13,30 +13,90 @@ Page({
         startDate:'',
         endDate:'',
         lists:'',
-        _id:'',
+        activityId:'',
         headcount:'',
-        // total:''
+        totals:''
+    },
+
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    async onLoad(options) {
+        this.setData({
+            activityId: options.id
+        })
+        let _this = this;
+        
+      let res= await wx.cloud.callFunction({
+            name:'activity',
+            data:{
+                type:'queryMyActivityList',
+                wholeActivity : true
+            }
+        })
+        res.result.data.forEach(el => {
+            if(_this.data.activityId == el._id){
+                let maxObj;
+                let arr = [];
+                arr.push(el);
+                let startDates = el.startDate.slice(5,10);
+                let endDates = el.endDate.slice(5,10);
+                let str = startDates;
+                let strr = endDates;
+                let strs = str.replace('-','.');
+                let strrs = strr.replace('-','.');
+                maxObj = JSON.parse(JSON.stringify(el.prizeNums)).sort((n,m)=>{
+                    return m.conditionsMet-n.conditionsMet
+                })[0];
+                let barNum = maxObj.conditionsMet;
+                _this.setData({
+                    activityDetail: arr,
+                    startDate: strs,
+                    endDate: strrs,
+                    lists: el.prizeNums,
+                    headcount: barNum
+                })
+            }
+        });
+        let result=await wx.cloud.callFunction({
+            name:'activity',
+            data:{
+                type:'getActivityCount',
+                activityId:_this.data.activityId
+            }
+        })
+        
+        let total = result.result.data.total;
+        let people = _this.data.headcount;
+        let bar = total / people * 100;
+        this.setData({
+            totals: bar
+        })
+        wx.showShareMenu({
+            withShareTicket: true,
+            menus:['shareAppMessage','shareTimeline']
+        })
+
+        
     },
     help(){
-        // console.log('11111');
-        console.log(this.data._id);
         wx.cloud.callFunction({
             name:'activity',
             data:{
                 type:'participateActivity',
-                activityId: this.data._id
+                activityId: this.data.activityId
             },
             success(res){
-                console.log(res);
                 if(res.result.status == 0){
                     wx.showToast({
-                        title: '已参加过活动',
+                        title: '活动已参加',
                         icon: 'error',
                         duration: 1500
                       })
                 } else {
                     wx.showToast({
-                        title: '已参加',
+                        title: '参加成功',
                         icon: 'success',
                         duration: 1500
                       })
@@ -60,86 +120,15 @@ Page({
           url: '/pages/home/index',
         })
       },
-
-      Datas:function (){
-          console.log('225525');
+      skip: function(rows){
+          wx.navigateTo({
+            url: `/pages/activity/goodsDetail/index`,
+            success: function(res) {
+              // 通过 eventChannel 向被打开页面传送数据
+              res.eventChannel.emit('acceptDataFromOpenerPage', { data: rows.currentTarget.dataset.item })
+            }
+          })
       },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    async onLoad(options) {
-        // console.log(options.id);
-        this.setData({
-            _id: options.id
-        })
-        this._id = options.id;
-        let _this = this;
-        
-      let res= await wx.cloud.callFunction({
-            name:'activity',
-            data:{
-                type:'queryMyActivityList',
-                wholeActivity : true
-            }
-        })
-        console.log(res.result);
-        res.result.data.forEach(el => {
-            // console.log(el.prizeNums);
-            // maxObj= el.prizeNums.sort((n,m)=>{
-            if(_this.data._id == el._id){
-                let maxObj;
-                let arrs = [];
-                // console.log(el);
-                arrs.push(el);
-                // console.log(arrs);
-                let startDates = el.startDate.slice(5,10);
-                let endDates = el.endDate.slice(5,10);
-                let str = startDates;
-                let strr = endDates;
-                let strs = str.replace('-','.');
-                let strrs = strr.replace('-','.');
-                // console.log(el.prizeNums);
-                // maxObj= el.prizeNums.sort((n,m)=>{
-                maxObj = JSON.parse(JSON.stringify(el.prizeNums)).sort((n,m)=>{
-                    return m.conditionsMet-n.conditionsMet
-                })[0];
-                let barNum = maxObj.conditionsMet;
-                _this.setData({
-                    activityDetail: arrs,
-                    startDate: strs,
-                    endDate: strrs,
-                    lists: el.prizeNums,
-                    headcount: barNum
-                })
-                // console.log(_this.data.activityDetail);
-            }
-        });
-        let res2=await wx.cloud.callFunction({
-            name:'activity',
-            data:{
-                type:'getActivityCount',
-                activityId:_this.data._id
-            }
-        })
-        
-        let total = res2.result.data.total;
-        // console.log(total);
-        // console.log(_this.data.headcount);
-        let people = _this.data.headcount;
-        // console.log(people);
-        let bar = total / people * 100;
-        console.log(bar);
-        this.setData({
-            total: bar
-        })
-        wx.showShareMenu({
-            withShareTicket: true,
-            menus:['shareAppMessage','shareTimeline']
-        })
-
-        
-    },
 
     /**
      * 生命周期函数--监听页面初次渲染完成
