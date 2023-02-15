34bfc2cb0a4d69d32a1a7bd240ad235b7508053a
F-17-2006 <3375926016@qq.com>
2023-02-15 20:55:30 +0800

    拉代码

--- cloudfunctions/changeActivityType/index.js
+++ cloudfunctions/changeActivityType/index.js
@@ -1,24 +1,69 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 
-cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
-const db = cloud.database();
-// 云函数入口函数
-exports.main = async (event, context) => {
-    let timestamp = Date.parse(new Date());
-    let date = new Date(timestamp);
-    let Y =date.getFullYear();
-    let M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
-    let D = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
-    // let H = date.getHours();
-    let time = Y+'-'+M+'-'+D;
-    console.log(time);
-    const wxContext = cloud.getWXContext()
-    // db.collection('activity').where({
 
-    // })
-    
-    return {
-        
-    }
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
+// 云函数入口函数
+const db = cloud.database();
+const _ = db.command
+exports.main = async (event, context) => {
+    let changeTyp1 = () => {
+        let nowTime = new Date().getTime();
+    return new Promise((resolve, reject) => {
+      db.collection('activity').where({
+        startDate: _.lt(nowTime)
+      }).update({
+        data: {
+          activityType: 0
+        }
+      }).then(res=>{
+        resolve(res);
+      }).catch(err=>{
+        reject(err);
+      })
+    })
+  }
+
+  let changeType2 = ()=>{
+    let nowTime = new Date().getTime();
+
+    return new Promise((resolve,reject)=>{
+      db.collection('activity').where({
+        startDate: _.gt(nowTime)
+      }).update({
+        data: {
+          activityType: 1,
+        }
+      }).then(res=>{
+        resolve(res);
+      }).catch(err=>{
+        reject(err);
+      })
+    })
+  }
+
+  let changeType3 = ()=>{
+    let nowTime = new Date().getTime();
+
+    return new Promise((resolve,reject)=>{
+      db.collection('activity').where({
+        endDate: _.gt(nowTime)
+      }).update({
+        data: {
+          activityType: 2
+        }
+      }).then(res=>{
+        resolve(res);
+      }).catch(err=>{
+        reject(err);
+      })
+    })
+  }
+  Promise.all([changeTyp1(),changeType2(),changeType3()]).then(res=>{
+    console.log('----------------我走到了更改的方法----------------');
+    console.log(res);
+    return res;
+  }).catch(err=>{
+    return err;
+  })
 }
