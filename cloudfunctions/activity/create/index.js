0cd771e252fa5b6084a516efbdda9c618887b23c
F-17-2006 <3375926016@qq.com>
2023-02-17 17:49:35 +0800

    拉代码

--- cloudfunctions/activity/create/index.js
+++ cloudfunctions/activity/create/index.js
@@ -12,17 +12,16 @@ exports.main = async (event, context) => {
     event.activityInfo.OPENID = wxContext.OPENID;
     event.activityInfo.activityType = 0;
     event.activityInfo.type = 0;//是否是推荐活动
-    event.activityInfo.startDate = Date.parse(event.activityInfo.startDate+":00");
-    event.activityInfo.endDate = Date.parse(event.activityInfo.endDate+":00");
     let createActivity = await db.collection('activity').add({
         data:  event.activityInfo
     });
+    console.log(createActivity);
     let res = {
         status:0,
         msg:"创建失败,请重新在试一试",
         data:[]
     }
-    if (createActivity._id) {
+    if (createActivity) {
         res.status = 200;
         res.msg = "创建成功";
     }
