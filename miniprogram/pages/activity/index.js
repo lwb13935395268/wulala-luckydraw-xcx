d42caa771bf81460357050fe610f1273993751b6
F-17-2006 <3375926016@qq.com>
2023-02-19 21:53:09 +0800

    拉蒂阿玛

--- miniprogram/pages/activity/index.js
+++ miniprogram/pages/activity/index.js
@@ -5,6 +5,8 @@ Page({
      * 页面的初始数据         
      */
     data: {
+        pageSize:10,//多少条数据
+        pageNum:1,//页数
         currentIndex: 0, //tab下标
         activityList:[],//活动列表0
         recommend:[],//推荐1
@@ -37,36 +39,54 @@ Page({
     },
     //下标
     onMyEvent:function(e){
-        let index = e.detail == undefined ? e : e.detail
         this.setData({
-            isShow:true
-        });
-        wx.cloud.callFunction({
-            name:'activity',
-            data:{
-                type:'listType',
-                listType:index,
-            },
-            success:(res)=>{
-                this.setData({
-                    activityList:res.result.data.data,
-                    isShow:false
-                });
-                this.data.activityList.forEach((item,index)=>{
-                    let startDate = "activityList["+index+"].startDate";
-                    let endDate = "activityList["+index+"].endDate";
-                    this.setData({
-                        [startDate]:new Date(item.startDate).toLocaleString().replace(/\//g,'-'),
-                        [endDate]:new Date(item.endDate).toLocaleString().replace(/\//g,'-')
-                    })
-                })
-            }
+           pageNum : 1,
+           activityList : []
         })
+        let index = e.detail == undefined ? e : e.detail;
+        this.setData({
+            isShow:true,
+            currentIndex:index
+        });
+        this.getActivityList(this.data.currentIndex)
     },
     loadImg:function(){
         this.setData({
             isShow:false,
         });
+    },
+    
+    //获取活动列表信息
+    getActivityList(index){
+        wx.cloud.callFunction({
+            name:'activity',
+            data:{
+                type:'listType',
+                listType:index,
+                pageSize:this.data.pageSize,
+                pageNum:this.data.pageNum,
+            }
+        }).then(res => {
+            let list = this.data.activityList.concat(res.result.data.data);
+            list.forEach((item,index)=>{
+                if (Number(item.startDate)) {
+                    item.startDate = new Date(item.startDate).toLocaleString().replace(/\//g,'-');
+                    item.endDate = new Date(item.endDate).toLocaleString().replace(/\//g,'-');
+                }
+            })
+            this.setData({
+                activityList:list,
+                isShow:false
+            });
+        })
+    },
+    // 滚动到底部触发的事件
+    scroll:function(){
+        let pageNum = this.data.pageNum+1;
+        this.setData({
+            pageNum:pageNum
+        })
+        this.getActivityList(this.data.currentIndex)
     },
     /**
      * 生命周期函数--监听页面加载
